0101f0696e503327f3ff372c543b974b006b3ba6
WoofMeister <56370697+WoofMeister@users.noreply.github.com>
2020-02-06 13:51:17 -0800

    Stars

--- Space.java
+++ Space.java
@@ -10,6 +10,8 @@ public class Space extends World
 {
     private Counter scoreCounter;
     private int startAsteroids = 3;
+    private int rgb;
+    private int starSize;
 
     /**
      * Create the space and all objects within it.
@@ -21,6 +23,7 @@ public class Space extends World
         background.setColor(Color.BLACK);
         background.fill();
         
+        
         Rocket rocket = new Rocket();
         addObject(rocket, getWidth()/2 + 100, getHeight()/2);
         
@@ -31,6 +34,12 @@ public class Space extends World
 
         Explosion.initializeImages();
         ProtonWave.initializeImages();
+        
+        rgb = Greenfoot.getRandomNumber(255);
+       // starSize = Greenfoot.getRandomNumber(5);
+        
+        paintStars(300);
+        
     }
     
     /**
@@ -45,6 +54,23 @@ public class Space extends World
             int y = Greenfoot.getRandomNumber(getHeight()/2);
             addObject(new Asteroid(), x, y);
         }
+    }
+    
+    private void paintStars(int count)
+    {
+        GreenfootImage background = getBackground();
+        background.setColor(new Color(rgb, rgb, rgb));
+        for (int i = 0; i < count; i++)
+        {
+            int x = Greenfoot.getRandomNumber(getWidth());
+            int y = Greenfoot.getRandomNumber(getHeight());
+            //rgb = Greenfoot.getRandomNumber(255);
+            starSize = Greenfoot.getRandomNumber(5);
+            
+            
+            background.fillOval(x, y, starSize, starSize);
+        }
+        
     }
     
     /**
